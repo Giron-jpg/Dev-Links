d9b9c8c02b1d0e20e96d531a106f20146a5f03c0
João Paulo Giron <joaopaulog964@gmail.com>
2024-08-13 16:08:17 -0300

    script
    toggle e cores

--- VSCode/interacao/script.js
+++ VSCode/interacao/script.js
@@ -1,7 +1,6 @@
 var trilho = document.getElementById('toggle')
-var corpo = document.querySelectorAll('body, #caixa1>img, div#toggle, div#toggle>i ,ul li a, ul li a:hover, .social-links>a>i, .links:hover, footer>p')
+var corpo = document.querySelectorAll('body, #caixa1>img, div#toggle, div#toggle>div#ball, div#toggle>i ,ul li a, ul li a:hover, .social-links>a>i, .links:hover, footer>p')
 trilho.addEventListener('click',()=>{
-    trilho.classList.toggle('claro')
     corpo.forEach(item=>{
         item.classList.toggle('claro')
     })
