9689c4703cdf216823d18aee4981cae2d7f65cde
João Paulo Giron <joaopaulog964@gmail.com>
2024-08-14 00:00:49 -0300

    scriipt
    ajustes texto

--- VSCode/interacao/script.js
+++ VSCode/interacao/script.js
@@ -1,5 +1,5 @@
 var trilho = document.getElementById('toggle')
-var corpo = document.querySelectorAll('body, #caixa1>img, div#toggle, div#toggle>div#ball, div#toggle>i ,ul li a, ul li a:hover, .social-links>a>i, .links:hover, footer>p')
+var corpo = document.querySelectorAll('body, #caixa1>img, div#toggle, div#toggle>div#ball, div#toggle>i, ul li a, ul li a:hover, .social-links>a>i, .links:hover, p#txt')
 trilho.addEventListener('click',()=>{
     corpo.forEach(item=>{
         item.classList.toggle('claro')
